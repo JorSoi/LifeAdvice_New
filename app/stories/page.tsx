ffcbf9c3afedce610fdbb91711b1c05786514877
Jorim Soika <jorim.soika@gmx.de>
2023-12-19 00:27:08 +0100

    new comments are now added to the commentList

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -1,10 +1,8 @@
 'use client'
 
 import BottomSheet from "@/components/global/BottomSheet/BottomSheet";
-import CommentLogicWrapper from "@/components/global/Comments/CommentLogicWrapper/CommentLogicWrapper";
 import MenuBar from "@/components/global/MenuBar/MenuBar";
 import Overlay from "@/components/global/Overlay/Overlay";
-import TextArea from "@/components/global/TextArea/TextArea";
 import SocialShareList from "@/components/pages/Home/SocialShareList/SocialShareList";
 
 function Stories() {
---
+++ components/global/Comments/TextArea/TextArea.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import { useEffect, useRef, useState } from 'react';
+import styles from './TextArea.module.scss'
+import supabaseBrowserClient from '@/lib/supabaseBrowserClient';
+import { CommentData } from '@/types/home.types';
+
+function TextArea({minLength, maxLength, lessonId, recipient, addToCommentList} : {minLength : number, maxLength : number, lessonId : number, recipient : string, addToCommentList : (param : CommentData) => void }) {
+
+    const [text, setText] = useState<string>('')
+    const [lengthWarning, setLengthWarning] = useState<'hidden' | 'warn' | 'stop'>('hidden');
+    const [isFocused, setIsFocused] = useState<boolean>(false);
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const supabase = supabaseBrowserClient();
+    const formRef = useRef<HTMLFormElement>(null);
+
+
+
+    const handleSubmit = async (e : React.FormEvent) => {
+        e.preventDefault();
+        setIsLoading(true);
+        const { data, error } = await supabase.from('comments').insert([{ 
+            lesson_id: lessonId, 
+            profile_id: 5, //must be dynamic
+            comment: text,
+        },]).select(`id, comment, upvotes, created_at, profiles(id, user_name, avatars(avatar_url))`).single();
+        if(!error) {
+            setIsLoading(false)
+            setText('') //also causes textarea to go back to default
+            const newComment : any = data;
+            addToCommentList(newComment)
+        } else {
+            console.log(error)
+        }
+    }
+
+
+    useEffect(() => {
+        // Checks if a recipient has been passed to this Component to open textarea and prefill it with the correct recipient
+        if(recipient) {
+            setText(`@${recipient} `)          
+        }
+        if (formRef.current) {
+            formRef.current.scrollIntoView(false) //"false" will align the bottom of the element to the bottom of the visible area which forces the formElement to be scrolled into the view as much as somewhat possible.
+            
+        }
+    }, [recipient])
+
+    useEffect(() => {
+        // Handles the styling of the lengthCounter
+        if(text.length > 0.8 * maxLength) {
+            setLengthWarning('stop');
+        } else if (text.length > 0.5 * maxLength) {
+            setLengthWarning('warn');
+        } else {
+            setLengthWarning('hidden');
+        }
+
+        //Ensures textarea remains expanded if there's text input
+        if(text.length > 0) {
+            setIsExpanded(true)
+        } else {
+            setIsExpanded(false)
+        }
+    }, [text])
+
+
+    return (
+        <form className={styles.textAreaWrapper} onSubmit={handleSubmit} ref={formRef}>
+            <textarea 
+                className={`${styles.textArea} ${isFocused || isExpanded ? styles.expanded : null}`} 
+                placeholder='Share your thoughts' 
+                value={text} 
+                onChange={({target}) => setText(target.value)} 
+                maxLength={maxLength} 
+                onFocus={
+                    () => {
+                        setIsFocused(true)
+                        if (formRef.current) {
+                            formRef.current.scrollIntoView(false) 
+                        }
+                    }
+                }
+                onBlur={() => setIsFocused(false)}
+            />
+
+            {
+                (isFocused || isExpanded) && <button className={`${styles.postButton}`} type={'submit'} disabled={text.length < minLength || isLoading}>Post{isLoading ? 'ing...' : null}</button>
+            }
+
+            <p className={`${styles.lengthCounter} ${lengthWarning == 'warn' ? styles.warn : lengthWarning == 'stop' ? styles.stop : styles.hidden}`}>
+                {text.length}/{maxLength}
+            </p>
+        </form>
+    );
+}
+
+export default TextArea;
